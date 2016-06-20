6edc889a3e74659a943171114b88a8fb152a1211
Felix Hamann <felix@hamann.xyz>
2016-06-20 17:51:23 +0200

    cleaned up the code

--- src/main/java/de/hsrm/mi/hamann/cluster/Verticle.java
+++ src/main/java/de/hsrm/mi/hamann/cluster/Verticle.java
@@ -3,16 +3,25 @@ package de.hsrm.mi.hamann.cluster;
 import java.util.function.Consumer;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.shareddata.AsyncMap;
 import io.vertx.core.shareddata.LocalMap;
 import io.vertx.core.shareddata.SharedData;
 
+/**
+ * Implements counters using different types of shared data
+ *
+ * @author Felix Hamann
+ *
+ */
 public class Verticle extends AbstractVerticle {
 	final private Logger log = LoggerFactory.getLogger(Verticle.class);
 	
@@ -26,6 +35,40 @@ public class Verticle extends AbstractVerticle {
 	}
 	
 	
+	// UTILITY
+	
+	private void write (HttpServerRequest req, int code, String msg) {
+		final String clen = Integer.toString(msg.getBytes().length);
+		
+		req.response()
+			.setStatusCode(code)
+			.putHeader(HttpHeaders.CONTENT_LENGTH, clen)
+			.putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+			.write(msg)
+			.end();
+	}
+	
+	
+	private <T> Handler<AsyncResult<T>> proxy (HttpServerRequest req, Consumer<T> handler) {
+		return res -> {
+			if (res.failed()) {
+				JsonObject msg = new JsonObject();
+				msg.put("error", res.cause().getMessage());
+				this.write(req, 500, msg.toString());
+			}
+			
+			handler.accept(res.result());
+		};
+	}
+	
+	
+	// SHARED DATA 
+	
+	/**
+	 * Return the counter saved in the vertx instances' local map
+	 * 
+	 * @return
+	 */
 	private Long getLocalCounter () {
 		final String name = "localcounter";
 		final String key = "counter";
@@ -40,7 +83,7 @@ public class Verticle extends AbstractVerticle {
 			local.put(key, (long) 0);
 		}
 		
-		// local race
+		// no local race?
 		Long counter = local.get(key);
 		local.put(key, counter + 1);
 		
@@ -49,98 +92,66 @@ public class Verticle extends AbstractVerticle {
 	
 	
 	
-	private void putClusterCounter (
-			AsyncMap<String, Long> cluster,
-			String key,
-			Long val,
-			Consumer<Long> handler
-	){
-		cluster.put(key, val, putRes -> {
-			if (putRes.failed()) {
-				throw new RuntimeException(putRes.cause());
-			}
-			handler.accept(val);
-		});
-	}
-	
-	private void getClusterCounter (Consumer<Long> handler) {
+	/**
+	 * Retrieve and return the current value of the cluster wide map
+	 * 
+	 * There is a race condition!
+	 * 
+	 * @param req
+	 * @param handler
+	 */
+	private void getClusterCounter (HttpServerRequest req, Consumer<Long> handler) {
 		final String name = "clustercounter";
 		final String key = "counter";
 		
 		// take cover - heavy elbow incoming!
 		
-		this.sd.<String, Long>getClusterWideMap(name, mapRes -> {
-			
-			if (mapRes.failed()) {
-				throw new RuntimeException(mapRes.cause());
-			}
-			
-			AsyncMap<String, Long> cluster = mapRes.result();
-			cluster.get(key, getRes -> {
+		this.sd.<String, Long>getClusterWideMap(name, this.proxy(req, cluster -> {
+			cluster.get(key, this.proxy(req, count -> {
 				
-				if (getRes.failed()) {
-					throw new RuntimeException(getRes.cause());
-				}
-				
-				// initialize
-				if (getRes.result() == null) {
-					this.putClusterCounter(cluster, key, (long) 1, handler);
-					
-				// increment
-				} else {
-					
-					// cluster-wide race
-					Long count = getRes.result().longValue() + 1;
-					this.putClusterCounter(cluster, key, count, handler);
-					
-				}
-			});
-			
-		});
+				if (count == null) { count = 0L; }
+				final Long val = count + 1L;
+				cluster.put(key, val, this.proxy(req, res -> handler.accept(val)));
+
+			}));
+		}));
 	}
 	
 	
-	private void getCounter (Consumer<Long> handler) {
+	/**
+	 * Increment and get a cluster wide counter
+	 * 
+	 * @param handler
+	 */
+	private void getCounter (HttpServerRequest req, Consumer<Long> handler) {
 		final String name = "counter";
 		
-		this.sd.getCounter(name, counterRes -> {
-			if (counterRes.failed()) {
-				throw new RuntimeException(counterRes.cause());
-			}
-			
-			counterRes.result().incrementAndGet(incRes -> {
-				if (incRes.failed()) {
-					throw new RuntimeException(incRes.cause());
-				}
-				
-				handler.accept(incRes.result());
-			});
-		});
+		this.sd.getCounter(name, this.proxy(req, counter -> {
+			counter.incrementAndGet(this.proxy(req, inc -> {
+				handler.accept(inc);
+			}));
+		}));
 	}
 	
 	
-	private void response (Consumer<String> handler) {
-		this.stats.setLocal(this.getLocalCounter());
-		this.getClusterCounter(clusterCounter -> {
-			this.stats.setCluster(clusterCounter);
-			this.getCounter(counter -> {
-				this.stats.setCounter(counter);
-				handler.accept(Json.encodePrettily(this.stats).toString());
-			});
-		});
-	}
-	
-	
+	/**
+	 * Handle everything (!)
+	 * 
+	 * @param req
+	 */
 	private void handle (HttpServerRequest req) {
-		this.response(msg -> {
-			final String clen = Integer.toString(msg.getBytes().length);
+		this.stats.setLocal(this.getLocalCounter());
 		
-			req.response()
-				.setStatusCode(200)
-				.putHeader(HttpHeaders.CONTENT_LENGTH, clen)
-				.putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-				.write(msg)
-				.end();
+		this.getClusterCounter(req, clusterCounter -> {
+			this.stats.setCluster(clusterCounter);
+			
+			this.getCounter(req, counter -> {
+				this.stats.setCounter(counter);
+				
+				String msg = Json.encodePrettily(this.stats).toString();
+				this.write(req, 200, msg);
+				
+			});
 		});
 	}
 	
@@ -162,8 +173,5 @@ public class Verticle extends AbstractVerticle {
 				fut.complete();
 			});
 	}
-
-	// TODO this.proxy for generic error handling
-	// TODO int -> Integer -> Long 
 	
 }
